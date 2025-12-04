0efc4cda239ef32231c1ccd0ede12d7de7a38caa
furkangumgol4-hub <furkangumgol4@gmail.com>
2025-12-04 17:09:01 +0530

    Update internet.py

--- internet.py
+++ internet.py
@@ -1,14 +1,31 @@
 import sys
-if len(sys.argv)!=5:
-    print("usage python internet.py <plan_name> <speed> <validity> <price>")
-    sys.exit(1)
-plan_name=sys.argv[1]
-speed=sys.argv[2]
-validity=sys.argv[3]
-price=sys.argv[4]
+default_plan = "Basic Plan"
+default_speed = "10Mbps"
+default_validity = "30Days"
+default_price = "500"
 
-print("___plan summary___")
-print("Plan name",plan_name)
-print("Speed",speed)
-print("Validity",validity)
-print("Price",price)
+if len(sys.argv) > 1:
+    plan_name = sys.argv[1]
+else:
+    plan_name = default_plan
+
+if len(sys.argv) > 2:
+    speed = sys.argv[2]
+else:
+    speed = default_speed
+
+if len(sys.argv) > 3:
+    validity = sys.argv[3]
+else:
+    validity = default_validity
+
+if len(sys.argv) > 4:
+    price = sys.argv[4]
+else:
+    price = default_price
+
+print("___ Plan Summary ___")
+print("Plan name:", plan_name)
+print("Speed:", speed)
+print("Validity:", validity)
+print("Price:", price)
