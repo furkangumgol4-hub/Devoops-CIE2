e04eb47bf54993038c7cb56a8195b28af5b3a38a
furkangumgol4-hub <furkangumgol4@gmail.com>
2025-12-04 17:34:36 +0530

    Update internet.py

--- internet.py
+++ internet.py
@@ -1,18 +1,19 @@
 import sys
-if len(sys.argv)!=5:
-plan_name=sys.argv[1]
-speed=sys.argv[2]
-validity=sys.argv[3]
-price=sys.argv[4]
+
+if len(sys.argv) == 5:
+    script_name = sys.argv[0]
+    plan_name = sys.argv[1]
+    speed = sys.argv[2]
+    validity = sys.argv[3]
+    price = sys.argv[4]
 else:
-script_name=sys.argv[0]
-plan_name="base"
-speed="10mbps"
-validity="3month"
-price="500"
+    plan_name = "base"
+    speed = "10mbps"
+    validity = "3month"
+    price = "500"
 
 print("___plan summary___")
-print("Plan name",plan_name)
-print("Speed",speed)
-print("Validity",validity)
-print("Price",price)
+print("Plan name:", plan_name)
+print("Speed:", speed)
+print("Validity:", validity)
+print("Price:", price)
